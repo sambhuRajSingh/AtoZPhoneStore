b5378125484698c099358b83fbf499df26a707ac
Mr Sambhu raj Singh <sambhu@sambhu.Home>
2017-06-02 13:05:32 +0100

    [FEATURE] - Add Databasse Migration

---
+++ database/migrations/2017_06_02_114351_create_phones_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code', 255);
+            $table->string('make', 50);
+            $table->string('model', 255);
+            $table->string('name', 255);
+            $table->string('type', 4);
+            $table->string('tar_code', 50);
+            $table->string('tar_name', 255);
+            $table->integer('tar_minutes')->unsigned();
+            $table->integer('tar_sms')->unsigned();;
+            $table->integer('tar_data')->unsigned();;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
